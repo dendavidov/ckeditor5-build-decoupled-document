daccf4c04907644681d46e1f28ddfafbf69a760d
Denis Davydov <denis.davydov@auto1.com>
2019-08-23 12:22:16 +0200

    add plugin

---
+++ src/ViewSourceButton.js
@@ -0,0 +1,7 @@
+import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+
+export class InsertImage extends Plugin {
+	init() {
+		console.log( 'InsertImage was initialized' );
+	}
+}
--- src/ckeditor.js
+++ src/ckeditor.js
@@ -33,6 +33,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
 import Table from '@ckeditor/ckeditor5-table/src/table';
 import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import ViewSourceButton from './ViewSourceButton';
 
 export default class DecoupledEditor extends DecoupledEditorBase {}
 
@@ -64,7 +65,8 @@ DecoupledEditor.builtinPlugins = [
 	Paragraph,
 	PasteFromOffice,
 	Table,
-	TableToolbar
+	TableToolbar,
+	ViewSourceButton
 ];
 
 // Editor configuration.

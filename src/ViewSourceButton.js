85df54d5c254b13e3ddf3bba243b7bfb9faab904
Denis Davydov <denis.davydov@auto1.com>
2019-08-23 12:28:38 +0200

    build

--- src/ViewSourceButton.js
+++ src/ViewSourceButton.js
@@ -1,6 +1,6 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
 
-export class InsertImage extends Plugin {
+export default class ViewSourceButton extends Plugin {
 	init() {
 		console.log( 'InsertImage was initialized' );
 	}
